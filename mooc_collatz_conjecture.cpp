3badc1c777e1d36f7f8be14bf6c8c4d1842824d1
Ghundo <marcavenzaid.gamil@gmail.com>
2018-09-28 04:43:05 +0800

    Added problem solutions

---
+++ mooc_collatz_conjecture.cpp
@@ -0,0 +1,24 @@
+/* This is only a temporary file and not one of the algorithm solutions.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int collatz_conjecture(int n, int steps) {
+    if (n == 1) {
+        return steps;
+    }
+    if (n % 2 == 0) {
+        return collatz_conjecture(n / 2, steps + 1);
+    } else {
+        return collatz_conjecture(3*n + 1, steps + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << collatz_conjecture(9, 0) << "\n";
+    return 0;
+}
---
+++ mooc_is_symmetrical.cpp
@@ -0,0 +1,25 @@
+/* This is only a temporary file and not one of the algorithm solutions.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_symmetrical(int a[], int l, int r) {
+    if (l >= r) {
+        return true;
+    }
+    if (a[l] != a[r]) {
+        return false;
+    }
+    return is_symmetrical(a, l + 1, r - 1);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int a[] = {1, 2, 3, 4, 4, 3, 2, 1};
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << is_symmetrical(a, 0, n - 1) << "\n";
+    return 0;
+}
